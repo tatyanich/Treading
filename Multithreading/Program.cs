3120f4af7fd7f37577b9565995641c0ab6a21f0f
tatyanich <tanya301293@gmail.com>
2014-10-10 12:36:42 +0400

    Change add method.

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -8,56 +8,136 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        static public void Multiply(Matrix mas, int a)
-        {
+        static public Block[][] PartitionBlocks(Matrix mas, int x, int y) {
             Block[][] blocks;
-            int x = 2;
-            int y = 2;
-                        int n;
+            int n;
             int m;
-
-            //Dobavila peremennue no vot kak izvlech dlinny Matrix mas ne uverenna chto pravilno
-            if (mas.Columns() % y > 0)
-            {
-                m = mas.Columns() / y + 1;
-            }
-            else
-            {
-                m = mas.Columns() / y;
-            }
-            if (mas.Rows() % x > 0)
-            {
-                n = mas.Rows() / x + 1;
-            }
-            else
-            {
-                n = mas.Rows() / x;
-            }
+                if (mas.Columns() % y > 0)
+                {
+                    m = mas.Columns() / y + 1;
+                }
+                else
+                {
+                    m = mas.Columns() / y;
+                }
+                if (mas.Rows() % x > 0)
+                {
+                    n = mas.Rows() / x + 1;
+                }
+                else
+                {
+                    n = mas.Rows() / x;
+                }
             blocks = new Block[n][];
 
-                    for (int i = 0; i < n; i++)
-                    {
-                        blocks[i] = new Block[m];
-                        for (int j = 0; j < m; j++)
-                        {
-                            blocks[i][j] = new Block();
-
-
-                        }
-                    }
-            for (int i = 0; i < mas.Rows();  i++)
-            {
-                for (int j = 0; j < mas.Columns(); j++)
+                for (int i = 0; i < n; i++)
                 {
-                    blocks[i / x][j / y].AddIndexes(new Index(i, j));
-                    
+                    blocks[i] = new Block[m];
+                    for (int j = 0; j < m; j++)
+                    {
+                        blocks[i][j] = new Block();
+
+
+                    }
                 }
-            }
-            double h;
+                for (int i = 0; i < mas.Rows(); i++)
+                {
+                    for (int j = 0; j < mas.Columns(); j++)
+                    {
+                        blocks[i / x][j / y].AddIndexes(new Index(i, j));
+
+                    }
+                }
+                return blocks;
+        }
+
+        //static public void Multiply(Matrix mas, int a)
+        //{
+        //    Block[][] blocks;
+        //    int x = 2;
+        //    int y = 2;
+        //                int n;
+        //    int m;
+
+            //Dobavila peremennue no vot kak izvlech dlinny Matrix mas ne uverenna chto pravilno
+            //if (mas.Columns() % y > 0)
+            //{
+            //    m = mas.Columns() / y + 1;
+            //}
+            //else
+            //{
+            //    m = mas.Columns() / y;
+            //}
+            //if (mas.Rows() % x > 0)
+            //{
+            //    n = mas.Rows() / x + 1;
+            //}
+            //else
+            //{
+            //    n = mas.Rows() / x;
+            //}
+            //blocks = new Block[n][];
+
+            //        for (int i = 0; i < n; i++)
+            //        {
+            //            blocks[i] = new Block[m];
+            //            for (int j = 0; j < m; j++)
+            //            {
+            //                blocks[i][j] = new Block();
+
+
+            //            }
+            //        }
+            //for (int i = 0; i < mas.Rows();  i++)
+            //{
+            //    for (int j = 0; j < mas.Columns(); j++)
+            //    {
+            //        blocks[i / x][j / y].AddIndexes(new Index(i, j));
+                    
+            //    }
+            //}
+            //double h;
             
-            for (int i = 0; i < n; i++)
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < m; j++)
+            //    {
+            //        for (int k = 0; k < blocks[i][j].Indexes.Count; k++)
+            //        {
+            //            Index ind = blocks[i][j].Indexes[k];
+
+            //            h = mas.GetElement(ind)*a;
+
+            //            mas.SetElement(h, ind);
+            //        }
+                
+
+            //    }
+            //}
+        
+              
+        //    System.Console.WriteLine("{0}{1}", n, m);
+        //    System.Console.WriteLine("{0}{1}", mas.Rows(), mas.Columns());
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = 0; j < m; j++)
+        //        {
+        //           System.Console.WriteLine("{0},{1}->{2}", i, j, blocks[i][j]); 
+
+        //        }
+        //    }
+
+        //    System.Console.WriteLine("{0}{1}", 0 / 2, 1 / 2);
+            
+        //}
+
+        static public void Multiply(Block[][] blocks, Matrix mas, int a)
+        {
+            double h;
+
+            for (int i = 0; i < blocks.Length; i++)
             {
-                for (int j = 0; j < m; j++)
+                for (int j = 0; j < blocks[i].Length; j++)
                 {
                     for (int k = 0; k < blocks[i][j].Indexes.Count; k++)
                     {
@@ -72,19 +152,6 @@ namespace ConsoleApplication1
                 }
             }
 
-            System.Console.WriteLine("{0}{1}", n, m);
-            System.Console.WriteLine("{0}{1}", mas.Rows(), mas.Columns());
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                   System.Console.WriteLine("{0},{1}->{2}", i, j, blocks[i][j]); 
-
-                }
-            }
-
-            System.Console.WriteLine("{0}{1}", 0 / 2, 1 / 2);
-            
         }
         private delegate double dUtreatMult(int a, double[][] matr);
 
@@ -103,8 +170,14 @@ namespace ConsoleApplication1
                };
 
             int b = 2;
+            int x = 2;
+            int y = 2;
             Matrix mass = new Matrix(arr);
-            Multiply(mass, b);
+
+
+            Block [][] tread;
+            tread = PartitionBlocks(mass, x, y);
+            Multiply(tread, mass,b);
 
 
             for (int i = 0; i <mass.Rows(); i++)
